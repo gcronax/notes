7b3512b9ee525148aeab97842deb635f8504e421
gcronax <guillecampher15@gmail.com>
2026-01-13 14:13:20 +0100

    a

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -144,11 +144,32 @@ fun topAppbarNotes(scrollBehavior: TopAppBarScrollBehavior) {
 @Composable
 fun ScrollContent( modifier: Modifier = Modifier) {
 
-    val listaCosas = listOf("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+    val listaCosas = listOf(
+        "Lorem Ipsum is simply dummy text of the printing and",
+        "typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever ",
+        "since the 1500s, when an unknown printer took a galley of type and scrambled it to make a ",
+        "type specimen book. It has survived not only five centuries, but also the leap into electronic ",
+        "typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of ",
+        "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software ",
+        "like Aldus PageMaker including versions of Lorem Ipsum.",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum ",
+        "Lorem Ipsum is simply dummy text of the printing and",
+        "typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever ",
+        "since the 1500s, when an unknown printer took a galley of type and scrambled it to make a ",
+        "type specimen book. It has survived not only five centuries, but also the leap into electronic ",
+        "typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of ",
+        "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software ",
+        "like Aldus PageMaker including versions of Lorem Ipsum.",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum ",
+        "Lorem Ipsum is simply dummy text of the printing and",
+        "typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever ",
+        "since the 1500s, when an unknown printer took a galley of type and scrambled it to make a ",
+        "type specimen book. It has survived not only five centuries, but also the leap into electronic ",
+        "typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of ",
+        "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software ",
+        "like Aldus PageMaker including versions of Lorem Ipsum.",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum ",
+    )
     LazyColumn (modifier.fillMaxSize()){
         items (listaCosas.size){ item->
 
